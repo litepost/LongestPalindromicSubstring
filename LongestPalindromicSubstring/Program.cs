730d91d0f6c3a231e2c9c19b18e340bc0fae1d23
litepost <litepost@gmail.com>
2024-05-05 15:35:54 -0700

    wires are hooked up

---
+++ LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://leetcode.com/problems/longest-palindromic-substring/description/
+
+static string LongestPalindrome(string s) {
+    string maxPalindrome = "";
+
+    return maxPalindrome;
+}
+
+string input = "babad";
+string answer = LongestPalindrome(input);
+System.Console.WriteLine($"The answer is '{answer}'");
