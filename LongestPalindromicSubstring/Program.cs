c07d000404ce1eda97d5817f7b27e3d5e3de52b2
litepost <litepost@gmail.com>
2024-05-05 17:38:05 -0700

    error when input = "a"

--- LongestPalindromicSubstring/Program.cs
+++ LongestPalindromicSubstring/Program.cs
@@ -2,10 +2,41 @@
 
 static string LongestPalindrome(string s) {
     string maxPalindrome = "";
+    System.Console.WriteLine($"s = {s}");
 
+    for (int i = 1; i < s.Length; i++) {
+        System.Console.WriteLine($"i = {i}");
+
+        // odd length palindrome
+        string oddPalindrome = Char.ToString(s[i]);
+        for (int j = 1; j <= Math.Min(i, s.Length - 1 - i); j++) {
+            System.Console.WriteLine($"s[i] = '{s[i]}'\tj = {j}\ts[i-j] = '{s[i-j]}'\ts[i+j] = '{s[i+j]}'");
+            if (s[i-j] == s[i+j]) {
+                oddPalindrome = Char.ToString(s[i-j]) + oddPalindrome + Char.ToString(s[i+j]);
+            }
+        }
+        if (oddPalindrome.Length > maxPalindrome.Length) {
+            maxPalindrome = oddPalindrome;
+        }
+
+        //even length palindrome
+        if (i < s.Length - 1 && s[i] == s[i+1]) {
+            string evenPalindrome = s.Substring(i,2);
+            for (int j = 1; j <= Math.Min(i, s.Length - 1 - (i + 1)); j++) {
+                if (s[i-j] == s[i+1+j]) {
+                    evenPalindrome = Char.ToString(s[i-j]) + evenPalindrome + Char.ToString(s[i+1+j]);
+                }
+            }
+            if (evenPalindrome.Length > maxPalindrome.Length) {
+                maxPalindrome = evenPalindrome;
+            }
+        }
+    }
     return maxPalindrome;
 }
 
-string input = "babad";
+// string input = "babad";
+// string input = "cbbd";
+string input = "cbbcd";
 string answer = LongestPalindrome(input);
 System.Console.WriteLine($"The answer is '{answer}'");
